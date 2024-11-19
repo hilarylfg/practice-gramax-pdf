2fd93283a4506c03dad5f1fc2dc0596f158dd220
Nikolay Polyakov <misterkola337@gmail.com>
2024-11-19 21:34:33 +0300

    fix inlineCode

--- src/cases/paragraphCase.ts
+++ src/cases/paragraphCase.ts
@@ -2,8 +2,16 @@ import { ASTNode } from '../../types/ASTNode';
 import {extractText} from "./extractTextForCases.ts";
 
 export function paragraphCase(node: ASTNode): any {
-    return {
-        text: extractText(node),
-        style: `paragraph`,
-    };
+    (node.content || []).map((item) => {
+        if (item.marks?.some((mark) => mark.type === 'code')) {
+            return {
+                text: extractText(node) || '',
+                style: 'inlineCodeBlock',
+                background: '#f5f5f5',
+            };
+        }
+        return { text: item.text || '' };
+    });
+
+    return { text: extractText(node), style: 'paragraph', margin: [0, 5, 0, 5] };
 }
