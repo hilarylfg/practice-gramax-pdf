6fb18172b89ead905ab4b42f5cfd3d7c9ed24407
Nikolay Polyakov <misterkola337@gmail.com>
2024-12-25 15:07:14 +0300

    remove tag in types

--- types/ASTNode.ts
+++ types/ASTNode.ts
@@ -5,21 +5,12 @@ export interface ASTNode {
         id?: string | null; // Уникальный идентификатор
         isCustomId?: boolean; // Индивидуальный идентификатор
         href?: string; // Ссылка
-        src?: string; // Источник (например, изображения)
+        src?: string; // Источник
         width?: number; // Ширина
         height?: number; // Высота
-        type?: string; // Тип (например, для заметок)
+        type?: string; // Тип
         title?: string; // Заголовок
-        path?: string; // Путь (например, для файлов)
-        tag?: { // Для inlineMd_component
-            $$mdtype: string; // Тип (например, Tag)
-            name: string; // Имя (например, Kbd)
-            attributes: { // Атрибуты тега
-                text?: string; // Текст внутри тега
-                [key: string]: string | number | undefined; // Дополнительные атрибуты
-            };
-            children: ASTNode[]; // Вложенные элементы
-        }[];
+        path?: string; // Путь
         text?: string; // Текстовое содержимое
         colspan?: number; // Объединение ячеек по столбцам
         rowspan?: number; // Объединение ячеек по строкам
